e3f7abc904439b50f8a456b5a11ccbf5b20f05c8
Paulo <pauloroberto.21s@gmail.com>
2018-06-28 23:51:42 +0000

    pequenas alterações em settings.py

--- iampaulo/settings.py
+++ iampaulo/settings.py
@@ -21,12 +21,12 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['secret_key']
+SECRET_KEY = os.environ.get('secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True 
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['www.pauloroberto.me', 'www.iampaulo.com']
 
 
 # Application definition
